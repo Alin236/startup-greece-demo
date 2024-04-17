f7c33c3b6fdf1b994344fab254a9165ed1ca473a
Alin236 <nilavan.deva@etu.imt-nord-europe.fr>
2024-04-17 21:27:05 +0200

    Comment fakeField.js

--- js/fakeField.js
+++ js/fakeField.js
@@ -1,6 +1,7 @@
+// This is an example of fields. It is also used at the launch of the page to avoid having empty forms and header's table.
 let fields = [
     {id: 1, type: 'text', name: 'name'},
     {id: 2, type: 'text', name: 'age'},
     {id: 3, type: 'select', name: 'color', options: ['red', 'blue', 'yellow', 'pink']},
-]
+];
 let nextId = fields[fields.length - 1].id + 1;
