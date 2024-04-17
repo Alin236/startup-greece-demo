2cdb4f340b91c5aa89c31e23e2237434d7e3d83d
Alin236 <nilavan.deva@etu.imt-nord-europe.fr>
2024-04-17 21:14:39 +0200

    Comment addField.js

--- js/addField.js
+++ js/addField.js
@@ -1,13 +1,17 @@
-let radioText = $('#radio-text');
+let radioText = $('#radio-text'); // Unused variable
 let radioSelect = $('#radio-select');
 let inputName = $('#field-name');
 let inputItems = $('#select-items');
 let separator = ';';
 
+// This event handler change the "Add field" form according to the type of field the user want to add.
 $('#add-field-form :radio').on('click', function(event){
     $('#select-items').prop('disabled', event.target.value != 'select');
 });
 
+// This function is called when the "Add field" form is submitted.
+// It gathers details about the new field that must be added and add it to the fields variable.
+// It also calls functions that recreate the custom form, the "Delete field" form and the data table.
 function submitAddFieldForm(){
     let field = {};
     let name = inputName.val();
@@ -21,21 +25,26 @@ function submitAddFieldForm(){
     }
     else
         type = 'text';
-    field.type = type
+
+    field.type = type;
     field.id = nextId;
     nextId++;
+
     fields.push(field);
+    
     createForm();
     createTable();
     createDeleteFieldForm();
     resetAddFieldForm();
 }
 
+// This event handler manage the submission of the "Add field" form.
 $('#add-field-form > button').on('click', function(event){
     event.preventDefault();
     submitAddFieldForm();
 })
 
+// This function cleans the fields of the "Add field" form.
 function resetAddFieldForm(){
     inputName.val('');
     inputItems.val('');
