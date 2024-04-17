150013f52830427a833c08392702e1d7b7f7a62f
Alin236 <nilavan.deva@etu.imt-nord-europe.fr>
2024-04-17 23:33:42 +0200

    Add .csv example

--- js/fakeField.js
+++ js/fakeField.js
@@ -1,7 +1,7 @@
 // This is an example of fields. It is also used at the launch of the page to avoid having empty forms and table's header.
 let fields = [
-    {id: 1, type: 'text', name: 'name'},
-    {id: 2, type: 'text', name: 'age'},
-    {id: 3, type: 'select', name: 'color', options: ['red', 'blue', 'yellow', 'pink']},
+    {id: 1, type: 'text', name: 'Name'},
+    {id: 2, type: 'text', name: 'Age'},
+    {id: 3, type: 'select', name: 'Color', options: ['Red', 'Blue', 'Yellow', 'Pink']},
 ];
 let nextId = fields[fields.length - 1].id + 1;
